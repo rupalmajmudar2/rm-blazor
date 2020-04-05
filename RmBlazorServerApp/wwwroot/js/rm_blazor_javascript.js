a2ae9595b4a027c70056a79d7e2b286a77bd6f44
rupal_vsc <rupal_majmudar@hotmail.com>
2020-04-05 15:07:19 +0200

    fetch js data

--- RmBlazorServerApp/wwwroot/js/rm_blazor_javascript.js
+++ RmBlazorServerApp/wwwroot/js/rm_blazor_javascript.js
@@ -1,3 +1,12 @@
-﻿function RmTestAlert() {
-    alert("Blazor JS Alert");
+﻿function RmTestAlert(p1, p2) {
+    alert("Blazor JS Alert with params: " + p1 + " and " + p2);
+}
+
+function RmTestDOMUpdate(p1, p2) {
+    document.getElementById('rmDivAuthors').innerText = "DOM Updated! With " + p1 + "&" + p2;
+}
+
+function GetCities() {
+    cities = ['Mumbai', 'Bangalore', 'Zurich']
+    return cities;
 }
